522b1916ca438edb75ea65faddc2e5729fafad2c
abdelrahmanghanem <abdelrahman.ghanem.atya@gmail.com>
2024-12-17 17:56:22 +0200

    upgrade 1.0.6

--- ios/Classes/delegates/LocationManagerDelegate.swift
+++ ios/Classes/delegates/LocationManagerDelegate.swift
@@ -15,11 +15,18 @@ class LocationManagerDelegate: NSObject, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion) {
         log.debug("didDetermineState: \(String(describing: state)) for geofence ID: \(region.identifier)")
 
-        guard let event: GeofenceEvent = switch state {
-        case .unknown: nil
-        case .inside: .enter
-        case .outside: .exit
-        } else {
+        // Fix the switch statement
+        let event: GeofenceEvent?
+        switch state {
+        case .unknown:
+            event = nil
+        case .inside:
+            event = .enter
+        case .outside:
+            event = .exit
+        }
+
+        guard let validEvent = event else {
             log.error("Unknown CLRegionState: \(String(describing: state))")
             return
         }
@@ -34,10 +41,9 @@ class LocationManagerDelegate: NSObject, CLLocationManagerDelegate {
             return
         }
 
-        let params = GeofenceCallbackParamsWire(geofences: [activeGeofence], event: event, callbackHandle: callbackHandle)
+        let params = GeofenceCallbackParamsWire(geofences: [activeGeofence], event: validEvent, callbackHandle: callbackHandle)
         nativeGeofenceBackgroundApi.geofenceTriggered(params: params)
     }
-
     func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: any Error) {
         log.error("monitoringDidFailFor: \(region?.identifier ?? "nil") withError: \(error)")
     }
